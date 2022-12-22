36bb1d6cc541138d691f6f98a19dde09d51fa1c1
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-12-22 15:27:46 +0900

    feat: add googleSignIn in auth context

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,10 +1,24 @@
 import { useContext, createContext } from "react";
+import { 
+    GoogleAuthProvider,
+    signInWithPopup,
+    signInWithRedirect,
+    signOut,
+    onAuthStateChanged  
+} from "firebase/auth";
+import {auth} from '../firebase/firebase';
 
 const AuthContext = createContext()
 
 export const AuthContextProvider = ({children}) => {
+
+    const googleSignIn = () => {
+        const provider = new GoogleAuthProvider()
+        signInWithPopup(auth,provider)
+    } 
+
     return (
-        <AuthContext.Provider value={{}}>
+        <AuthContext.Provider value={{googleSignIn}}>
             {children}
         </AuthContext.Provider>
     )
