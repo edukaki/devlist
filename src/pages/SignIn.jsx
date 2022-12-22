013748577462ce7002a1652cc5090d595643b202
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-12-22 13:00:26 +0900

    feat(signin): add google sign in function

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,11 +1,24 @@
 import GoogleButton from "react-google-button";
+import { UserAuth } from "../context/AuthContext";
 
 const SignIn = () => {
+
+    const { googleSignIn } = UserAuth();
+
+    const handleGoogleSignIn = async () => {
+        try{
+            await googleSignIn()
+        }
+        catch(error){
+            console.log(error)
+        }
+    }
+
     return (
         <>
         <div className="text-center text-3xl font-bold py-8">SignIn</div>
         <div className="max-w-[240px] m-auto py-4">
-            <GoogleButton />
+            <GoogleButton onClick={handleGoogleSignIn} />
         </div>
         </>
     )
