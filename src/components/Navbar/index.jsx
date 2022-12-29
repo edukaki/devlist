c18c5707ae0925bce736bc06fe357942e1a8d15d
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-12-29 23:59:09 +0900

    feat(navbar): add users features

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const Navbar = (props) => {
+
+    const [user, setUser] = useState('')
+
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            setUser(user)
+        } else {
+            setUser(null)
+        }
+    });
 
     return (
 
@@ -11,27 +23,32 @@ const Navbar = (props) => {
                 <Link to='/' className="flex items-center">
                     <span className="self-center text-xl font-semibold whitespace-nowrap">DevNote</span>
                 </Link>
-                <div className="flex items-center md:order-2">
-                    <button type="button" className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
-                        <img className="w-8 h-8 rounded-full" src={props.avatar} alt="user" />
-                    </button>
-                    <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow" id="user-dropdown">
-                        <ul className="py-1" aria-labelledby="user-menu-button">
-                            <li>
-                                <Link to='' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Dashboard</Link>
-                            </li>
-                            <li>
-                                <Link to='' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Settings</Link>
-                            </li>
-                            <li>
-                                <Link to='' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Sign out</Link>
-                            </li>
-                        </ul>
+                {user ?
+                    <div className="flex items-center md:order-2">
+                        <button type="button" className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                            <img className="w-8 h-8 rounded-full" src={user.photoURL} alt="user" />
+                        </button>
+                        <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow" id="user-dropdown">
+                            <ul className="py-1" aria-labelledby="user-menu-button">
+                                <li>
+                                    <Link to='' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Dashboard</Link>
+                                </li>
+                                <li>
+                                    <Link to='' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Settings</Link>
+                                </li>
+                                <li>
+                                    <Link to='' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Sign out</Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="mobile-menu-2" aria-expanded="false">
+                            <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+                        </button>
                     </div>
-                    <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="mobile-menu-2" aria-expanded="false">
-                        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
-                    </button>
-                </div>
+                    : <Link to='/signin'>
+                        Sign in
+                    </Link>
+                }
                 <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
                     <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
                         <li>
