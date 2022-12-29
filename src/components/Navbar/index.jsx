3d5975ada7f6d0b569700573c07ea147c872e45e
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-12-30 01:45:25 +0900

    feat: format sign in button

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -31,7 +31,7 @@ const Navbar = (props) => {
                         <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow" id="user-dropdown">
                             <ul className="py-1" aria-labelledby="user-menu-button">
                                 <li>
-                                    <Link to='' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Dashboard</Link>
+                                    <Link to='/' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Dashboard</Link>
                                 </li>
                                 <li>
                                     <Link to='' className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Settings</Link>
@@ -45,9 +45,12 @@ const Navbar = (props) => {
                             <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                         </button>
                     </div>
-                    : <Link to='/signin'>
+                    : 
+                    <div className="flex items-center md:order-2">
+                    <Link to='/signin'>
                         Sign in
                     </Link>
+                    </div>
                 }
                 <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
                     <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
