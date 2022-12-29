7202879b3a1794037cc04bf4e1c14b05959203ec
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2022-12-30 01:49:57 +0900

    refactor(navbar): get the user from context

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,19 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { UserAuth } from '../../context/AuthContext';
 
-const Navbar = (props) => {
+const Navbar = () => {
 
-    const [user, setUser] = useState('')
-
-    const auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
-        if (user) {
-            setUser(user)
-        } else {
-            setUser(null)
-        }
-    });
+    const { user } = UserAuth()
 
     return (
 
@@ -23,7 +14,13 @@ const Navbar = (props) => {
                 <Link to='/' className="flex items-center">
                     <span className="self-center text-xl font-semibold whitespace-nowrap">DevNote</span>
                 </Link>
-                {user ?
+                {!user ?
+                    <div className="flex items-center md:order-2">
+                    <Link to='/signin'>
+                        Sign in
+                    </Link>
+                    </div>
+                    : 
                     <div className="flex items-center md:order-2">
                         <button type="button" className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                             <img className="w-8 h-8 rounded-full" src={user.photoURL} alt="user" />
@@ -44,12 +41,6 @@ const Navbar = (props) => {
                         <button data-collapse-toggle="mobile-menu-2" type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="mobile-menu-2" aria-expanded="false">
                             <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                         </button>
-                    </div>
-                    : 
-                    <div className="flex items-center md:order-2">
-                    <Link to='/signin'>
-                        Sign in
-                    </Link>
                     </div>
                 }
                 <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="mobile-menu-2">
