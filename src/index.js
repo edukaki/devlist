935184c45bac2d2632144570c5cc660e04b1f154
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-01-02 08:10:07 +0900

    feat(index): include project context

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './Routes/router';
 import { AuthContextProvider } from './context/AuthContext';
 import './index.css';
+import { ProjectContextProvider } from './context/ProjectsContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <RouterProvider router={router} />
+      <ProjectContextProvider>
+        <RouterProvider router={router} />
+      </ProjectContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
