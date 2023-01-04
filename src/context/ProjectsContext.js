f0b1696bc5d841425a106650603153e6c49ca7bf
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-01-04 10:46:26 +0900

    feat(projectContext): create function to insert data into database

--- src/context/ProjectsContext.js
+++ src/context/ProjectsContext.js
@@ -1,14 +1,25 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import { db } from '../firebase/firebase'
+import { doc, setDoc } from 'firebase/firestore'
+import { UserAuth } from "./AuthContext";
 
 const ProjectContext = createContext()
 
 
 export const ProjectContextProvider = ({ children }) => {
+    const { user } = UserAuth()
+    const [project, setProject] = useState(null)
     const [projectArr, setProjectArr] = useState([])
 
 
+    useEffect(() => {
+        if (project) {
+            setDoc(doc(db, 'users', user.uid, 'projects', project.key), project)
+        }
+    }, [project, user.uid])
+
     return (
-        <ProjectContext.Provider value={{projectArr}}>
+        <ProjectContext.Provider value={{ setProject, projectArr }}>
             {children}
         </ProjectContext.Provider>
     )
