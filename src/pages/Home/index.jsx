0d7ec9f92edec67f98a777f23f2efacb95b2c496
Carol A. Nikolic <carolnikolic@gmail.com>
2023-01-06 23:04:55 -0300

    feat(home): add envelop section to devnote description

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,16 +4,18 @@ import heroImageHome from '../../img/home/hero-img.png'
 const Home = () => {
     return (
         <main className='min-h-[100vh]'>
-            <div>
-                <h1>DevNote</h1>
-                <p>The ultimate <span>FREE </span><span>developer</span> tool to keep track of your projects</p>
-                <input type="email" placeholder="email"></input>
-                <button type="button">Register</button>
-            </div>
+            <section>
+                <div>
+                    <h1>DevNote</h1>
+                    <p>The ultimate <span>FREE </span><span>developer</span> tool to keep track of your projects</p>
+                    <input type="email" placeholder="email"></input>
+                    <button type="button">Register</button>
+                </div>
 
-            <div>
-                <img src={heroImageHome} alt="decorative Devnote features"/>
-            </div>
+                <div>
+                    <img src={heroImageHome} alt="decorative Devnote features"/>
+                </div>
+            </section>
         </main>
     )
 }
