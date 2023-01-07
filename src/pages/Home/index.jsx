632279ad3a5085837bd65d6bf3e4401bf26cb2b9
Carol A. Nikolic <carolnikolic@gmail.com>
2023-01-06 21:16:03 -0300

    feat(home): add introduction to devnote and register input

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Home = () => {
     return (
         <main className='min-h-[100vh]'>
-            Test
+            <div>
+                <h1>DevNote</h1>
+                <p>The ultimate <span>FREE </span><span>developer</span> tool to keep track of your projects</p>
+                <input type="email" placeholder="email"></input>
+                <button type="button">Register</button>
+            </div>
         </main>
     )
 }
