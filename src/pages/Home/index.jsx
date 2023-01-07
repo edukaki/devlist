d3e1fe827796c0662c77eb99186ba4390152e351
Carol A. Nikolic <carolnikolic@gmail.com>
2023-01-06 22:14:11 -0300

    feat(home): import and add hero image

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import heroImageHome from '../../img/home/hero-img.png'
 
 const Home = () => {
     return (
@@ -8,6 +9,10 @@ const Home = () => {
                 <p>The ultimate <span>FREE </span><span>developer</span> tool to keep track of your projects</p>
                 <input type="email" placeholder="email"></input>
                 <button type="button">Register</button>
+            </div>
+
+            <div>
+                <img src={heroImageHome} alt="decorative Devnote features"/>
             </div>
         </main>
     )
