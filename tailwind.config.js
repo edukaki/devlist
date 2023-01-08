748842faaf954fdf38b81fa46df12daacefb841e
Carol A. Nikolic <carolnikolic@gmail.com>
2023-01-08 16:56:18 -0300

    feat(tailwind config): add brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,12 @@ module.exports = {
     screens: {
       'larger': '768px'
     },
+    colors: {
+      darkGray: '#424040',
+      orange: '#FFA500',
+      blue: '#009EFF',
+      green: '#69B848',
+    },
     extend: {},
     fontFamily: {
       courier: ["Courier Prime", "monospace"],
