b77809847bb1f46dd8ccbe70459bbaa5ba6b0482
Carol A. Nikolic <carolnikolic@gmail.com>
2023-01-09 01:36:21 -0300

    feat(home): resize text for large screens

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,11 +7,11 @@ import remainingTimeIcon from '../../img/icons/remaining-icon.png'
 
 const Home = () => {
     return (
-        <main className='min-h-[100vh]'>
+        <main className='min-h-[100vh] scroll-smooth'>
             <section className='flex flex-col-reverse justify-around pt-6 md:flex-row text-darkGray'>
-                <div className='flex flex-col tracking-wider font-roboto space-y-6 md:w-6/12 xl:w-4/12 my-auto p-5'>
-                    <h1 className="font-courier font-bold text-4xl text-center pt-3 md:text-5xl md:text-left">DevNote</h1>
-                    <p className="text-xl text-center m-auto pt-6 sm:w-2/3 md:text-left md:m-0">The ultimate <span className="font-courier uppercase font-bold text-orange">free</span> <span className="font-courier font-bold text-orange">developer</span> tool to keep track of your projects</p>
+                <div className='flex flex-col tracking-wider font-roboto space-y-6 md:w-6/12 xl:w-5/12 my-auto p-5'>
+                    <h1 className="font-courier font-bold text-4xl text-center pt-3 lg:text-5xl md:text-left lg:text-6xl">DevNote</h1>
+                    <p className="text-xl text-center m-auto pt-6 sm:w-2/3 md:text-left md:m-0 lg:text-4xl">The ultimate <span className="font-courier uppercase font-bold text-orange">free</span> <span className="font-courier font-bold text-orange">developer</span> tool to keep track of your projects</p>
                     <input className="drop-shadow rounded w-2/5 place-self-center py-1 px-2 md:place-self-start lg:w-3/4" type="email" placeholder="email"></input>
                     <button className="bg-blue rounded-lg w-auto place-self-center py-2 px-3 text-white md:place-self-start lg:w-1/2" type="button">Register</button>
                 </div>
