f2116ee75e9a36b2fd9eb5408c7f731c7c9c8497
Carol A. Nikolic <carolnikolic@gmail.com>
2023-01-11 01:55:30 -0300

    feat(components): add new style to generic button

--- src/components/ButtonGeneric/index.jsx
+++ src/components/ButtonGeneric/index.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const ButtonGeneric = (props) => {
     return (
-        <button className={`${props.bgColor} rounded-lg w-auto place-self-center py-2 px-3 ${props.textColor} md:place-self-start lg:w-1/2`}
+        <button 
+            className={`${props.bgColor} hover:-translate-y-0.5 transform focus:outline-none focus:ring focus:ring-offset-2 focus:${props.ringColor} focus:ring-opacity-50 rounded-lg place-self-center py-2 px-3 ${props.textColor} ${props.textColor} md:${props.placeSelf} lg:${props.width}`}
             type="button">{props.buttonName}</button>
     )
 }
+
 export default ButtonGeneric
