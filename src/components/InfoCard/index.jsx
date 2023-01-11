e79b3110a5b0af73a0d750f0309df7e66eb3853a
Carol A. Nikolic <carolnikolic@gmail.com>
2023-01-10 23:28:17 -0300

    feat(home): place items on center

--- src/components/InfoCard/index.jsx
+++ src/components/InfoCard/index.jsx
@@ -5,8 +5,8 @@ const InfoCard = (props) => {
 
         <div className="flex flex-col">
             <img className="w-36 place-self-center" src={props.src} alt={props.alt} />
-            <h3 className="w-48 text-center pb-4 font-courier font-semibold tracking-wider md:text-left">{props.title}</h3>
-            <p className="w-48 text-center md:text-left tracking-wider lg:w-40">{props.description}</p>
+            <h3 className="w-48 place-self-center text-center pb-4 font-courier font-semibold tracking-wider md:text-left">{props.title}</h3>
+            <p className="w-48 place-self-center text-center md:text-left tracking-wider lg:w-40">{props.description}</p>
         </div>
 
     )
