b9b5e2911d84dedfa2e681aad9f581c72cbe813b
Carol A. Nikolic <carolnikolic@gmail.com>
2023-01-12 01:29:40 -0300

    feat(about): give to the headings font type, color, weight and size

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -2,14 +2,14 @@ const About = () => {
     return (
         <main>
             <div>
-                <h1>About DevNote</h1>
+                <h1 className="font-courier text-2xl font-bold text-darkGray">About DevNote</h1>
 
-                <h2>Time Is to Be Invested, Not Spent</h2>
+                <h2 className="font-courier text-lg font-semibold text-darkGray">Time Is to Be Invested, Not Spent</h2>
                 <p>DevNote is the only developer tool created to help you keep track of your projects, check what you have done and manage your time.</p>
             </div>
 
             <div>
-                <h2>The History Behind</h2>
+                <h2 className="font-courier text-lg font-semibold text-darkGray">The History Behind</h2>
                 <ul>
                     <li>In 2022 Eduardo Arakaki came up with the idea of creating a personal project management tool based on his need to keep track of his developer projects.</li>
                     <li>In the same year, he invested his time in creating mechanisms to bring the idea live. DevNote was born.</li>
