dca4df5870490e7a4a0634ab7f85e541c432deba
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-01-12 11:58:39 +0900

    style: uncomment footer component

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom"
+import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
 
 const Layout = () => {
@@ -6,7 +7,7 @@ const Layout = () => {
         <>
             <Navbar />
             <Outlet />
-            {/* <Footer /> */}
+            <Footer />
         </>
     )
 }
