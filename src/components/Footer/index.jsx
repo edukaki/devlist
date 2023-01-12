0635b75ffa94d273a18448354bdd20e70a8e4436
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-01-12 15:48:36 +0900

    feat: format footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,14 +4,17 @@ import logo from '../../img/icons/logo.png'
 
 const Footer = () => {
   return (
-    	<footer className='bg-logoGray'>
-            <img src={logo} alt="Devnote logo" />
-            <ul className='pl-6 font-bold'>
-                <li className='py-10 '><Link to='/about'>About</Link></li>
-                <li><Link to='/contact'>Contact</Link></li>
-            </ul>
-            <p className='text-center py-5'>© Copyright 2023. Made by Edu e Carol</p>
-        </footer>
+    <footer className='bg-[#FFBD59]'>
+      <img className='w-1/6' src={logo} alt="Devnote logo" />
+      <ul className='pl-6 font-bold text-base'>
+        <li className='pt-7 pb-3'><Link to='/contact'>Home</Link></li>
+        <li className='pb-3'><Link to='/dashboard'>Dashboard</Link></li>
+        <li className='pb-3'><Link to='/about'>About</Link></li>
+        <li className='pb-3'><Link to='/contact'>Contact</Link></li>
+        <li className='pb-3'><Link to='/faq'>FAQ</Link></li>
+      </ul>
+      <p className='text-sm text-center py-5'>© Copyright 2023. Made by Edu e Carol</p>
+    </footer>
   )
 }
 
