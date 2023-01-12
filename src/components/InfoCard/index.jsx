894ae7192034f46948337130de55d0716af4b2cb
Carol A. Nikolic <carolnikolic@gmail.com>
2023-01-11 23:29:00 -0300

    feat(home): add margin to benefits cards

--- src/components/InfoCard/index.jsx
+++ src/components/InfoCard/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const InfoCard = (props) => {
     return (
 
-        <div className="flex flex-col px-3 pb-14 bg-grayish rounded-xl drop-shadow-md hover:scale-125 transition duration-300 easy-in-out">
+        <div className="flex flex-col px-3 mx-16 my-10 pb-14 bg-grayish rounded-xl drop-shadow-md hover:scale-125 transition duration-300 easy-in-out lg:m-0">
             <img className="w-36 place-self-center" src={props.src} alt={props.alt} />
             <h3 className="w-48 place-self-center text-center pb-4 font-courier font-semibold tracking-wider">{props.title}</h3>
             <p className="w-48 line-clamp-3 place-self-center text-center tracking-wider lg:w-40">{props.description}</p>
