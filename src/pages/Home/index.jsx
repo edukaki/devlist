4d72433b77d045d95b8ebeea90b16f5b12120dc8
Carol A. Nikolic <carolnikolic@gmail.com>
2023-01-11 23:54:55 -0300

    feat(home): replace inputs with input component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,6 +7,7 @@ import totalTimeIcon from '../../img/icons/clock-icon.png'
 import remainingTimeIcon from '../../img/icons/remaining-icon.png'
 import InfoCard from '../../components/InfoCard'
 import ButtonGeneric from '../../components/ButtonGeneric'
+import InputGeneric from '../../components/InputGeneric'
 
 const Home = () => {
     return (
@@ -21,7 +22,8 @@ const Home = () => {
                 <div className='flex flex-col tracking-wider font-roboto space-y-6 p-5 ml-12 md:absolute md:top-10 left-10 md:w-6/12 xl:w-5/12 my-auto'>
                     <h1 className="font-courier font-bold text-4xl text-center pt-3 lg:text-5xl md:text-left lg:text-6xl">DevNote</h1>
                     <p className="text-xl text-center m-auto pt-6 sm:w-2/3 md:text-left md:m-0 lg:text-4xl">The ultimate <span className="font-courier uppercase font-bold text-orange">free</span> <span className="font-courier font-bold text-orange">developer</span> tool to keep track of your projects</p>
-                    <input className="drop-shadow rounded w-2/5 place-self-center py-2 px-3 md:place-self-start lg:w-3/4" type="email" placeholder="email"></input>
+
+                    <InputGeneric placeSelf="place-self-start" width="w-3/4" type="input" placeholder="email"></InputGeneric>
 
                     <ButtonGeneric bgColor="bg-blue" hoverColor="bg-sky-400" textColor="text-white" placeSelf="md:place-self-start" ringColor="ring-blue" buttonName="Register" width="w-1/2">
                     </ButtonGeneric>
@@ -67,7 +69,9 @@ const Home = () => {
 
             <section className="my-24 flex flex-col w-6/12 space-y-6 items-center m-auto font-roboto md:text-left">
                 <h2 className="mb-8 text-2xl tracking-wider">Try it out! It's <span className="font-courier text-orange font-semibold">free!</span></h2>
-                <input className="drop-shadow rounded w-8/12 place-self-center m-auto py-1 px-2 lg:w-6/12" type="email" placeholder="email"></input>
+
+                <InputGeneric placeSelf="place-self-center" margin="m-auto" width="w-6/12" type="input" placeholder="email"></InputGeneric>
+
                 <ButtonGeneric bgColor="bg-blue" hoverColor="bg-sky-400" textColor="text-white" placeSelf="place-self-start" ringColor="ring-blue" buttonName="Register" width="w-3/12">
                 </ButtonGeneric>
             </section>
