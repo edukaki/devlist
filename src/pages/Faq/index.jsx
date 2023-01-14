2119f580582b3739f5e50a2386ca4d96f127ae4d
Carol A. Nikolic <carolnikolic@gmail.com>
2023-01-14 00:35:28 -0300

    feat(faq): create question btn component and add to faq page

--- src/pages/Faq/index.jsx
+++ src/pages/Faq/index.jsx
@@ -1,6 +1,6 @@
 import HeadingOne from "../../components/HeadingOne"
-import HeadingTwo from "../../components/HeadingTwo"
 import ParagraphBlock from "../../components/ParagraphBlock"
+import ButtonQuestion from "../../components/ButtonQuestion"
 import Ruler from "../../components/Ruler"
 
 const Faq = () => {
@@ -8,24 +8,26 @@ const Faq = () => {
         <main>
             
             <div className="w-6/12 m-auto py-16 px-8 space-y-6 tracking-wider leading-8 lg:w-9/12 lg:ml-10 lg:px-24 xl:mx-96 xl:12/12 ">
-                <HeadingOne headingOneContent="Frequently Asked Questions"></HeadingOne>
-
-                <button className="text-left"><HeadingTwo headingTwoContent="How do I add my projects to DevNote?"></HeadingTwo>  <i class="fa-solid fa-caret-down"></i></button>
-
-                <ParagraphBlock bgColor="exist" hidden="true" paragraphContent="Add you project do DevNote by hitting the plus sign above your cards in your dashboard. Fill in the blank spaces and Devnote will sync automatically!"></ParagraphBlock>
-
-                <Ruler></Ruler>
-
-                <button className="text-left"><HeadingTwo headingTwoContent="How do DevNote keep track of the time spent on each project?"></HeadingTwo>  <i class="fa-solid fa-caret-down"></i></button>
-
-                <ParagraphBlock bgColor="exist" hidden="true" paragraphContent="In order to keep track of the time spent on each project, you simply need to change the status of your ongoing project from paused to in progress. DevNote will calcutae and display the invested time."></ParagraphBlock>
                 
-                <Ruler></Ruler>
+                <HeadingOne headingOneContent="Frequently Asked Questions"/>
 
-                <button className="text-left"><HeadingTwo headingTwoContent="Do I have to pay in order to use DevNote?"></HeadingTwo>  <i class="fa-solid fa-caret-down"></i></button>
+                <ButtonQuestion hidden='true' text="How do I add my projects to DevNote?"/>
 
-                <ParagraphBlock bgColor="exist" hidden="true" paragraphContent="No. DevNote is completely FREE!"></ParagraphBlock>
-                </div>
+                <ParagraphBlock bgColor="exist" hidden="true" paragraphContent="Add you project do DevNote by hitting the plus sign above your cards in your dashboard. Fill in the blank spaces and Devnote will sync automatically!"/>
+
+                <Ruler/>
+                
+                <ButtonQuestion hidden='true' text="How do DevNote keep track of the time spent on each project?"/>
+                
+                <ParagraphBlock bgColor="exist" hidden="true" paragraphContent="In order to keep track of the time spent on each project, you simply need to change the status of your ongoing project from paused to in progress. DevNote will calcutae and display the invested time."/>
+                
+                <Ruler/>
+
+                <ButtonQuestion hidden='true' text="Do I have to pay in order to use DevNote?"/>
+
+                <ParagraphBlock bgColor="exist" hidden="true" paragraphContent="No. DevNote is completely FREE!"/>
+
+            </div>
            
         </main>
     )
