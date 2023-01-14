d75ed6cd759d2b5ce6314770d746b2762fafb1b9
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-01-14 16:42:12 +0900

    feat(addProjectForm): add initial values object

--- src/components/AddProjectForm/index.jsx
+++ src/components/AddProjectForm/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useFormik } from 'formik';
+import { v4 as uuid } from 'uuid'
 
 const AddProjectForm = () => {
+    const formik = useFormik({
+        initialValues: {
+            key: uuid(),
+            project: '',
+            client: '',
+            status: '',
+            start: '',
+            deadline: '',
+        },
 
+    });
     return (
-        <form>
+        <form onSubmit={formik.handleSubmit}>
 
         </form>
     );
