8d047a830e9d805aaedb28c1297fc22e8cf86063
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-01-16 08:10:36 +0900

    style(buttonQuestion): format to be more readable

--- src/components/ButtonQuestion/index.jsx
+++ src/components/ButtonQuestion/index.jsx
@@ -4,7 +4,11 @@ import HeadingTwo from '../HeadingTwo';
 const ButtonQuestion = (props) => {
     return (
         <button className="text-left">
-            <HeadingTwo headingTwoContent={props.text}></HeadingTwo> {props.hidden === 'true' ? <i class="fa-solid fa-caret-down"></i> : <i class="fa-solid fa-caret-up"></i>}
+            <HeadingTwo headingTwoContent={props.text}></HeadingTwo>
+            {props.hidden === 'true' ?
+                <i class="fa-solid fa-caret-down"></i> :
+                <i class="fa-solid fa-caret-up"></i>
+            }
         </button>
     )
 }
