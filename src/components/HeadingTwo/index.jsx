b8f90a0b65114743fc3f4bb799f3756bee9704e7
Carol A. Nikolic <carolnikolic@gmail.com>
2023-01-15 03:02:24 -0300

    feat: change pseudo element position in h2 component

--- src/components/HeadingTwo/index.jsx
+++ src/components/HeadingTwo/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HeadingTwo = (props) => {
     return (
-        <h2 className={`inline font-courier text-left text-xl font-semibold text-darkGray ${props.position === 'relative' ? 'relative before:absolute before:h-4 before:w-20 before:bg-orange before:top-14 md:before:top-8 hover:before:bg-gradient-to-r from-orange to-green mb-10' : props.position}`}>{props.headingTwoContent}</h2>
+        <h2 className={`inline font-courier text-left text-xl font-semibold text-darkGray ${props.position === 'relative' ? 'relative before:absolute before:h-4 before:w-20 before:bg-orange before:top-10 md:before:top-8 hover:before:bg-gradient-to-r from-orange to-green mb-10' : props.position}`}>{props.headingTwoContent}</h2>
     )
 }
 
