72007d4d081ccefe1e9255f8b06982b57ec4d0bf
Carol A. Nikolic <carolnikolic@gmail.com>
2023-01-15 03:24:19 -0300

    feat(about): ajust text width and pseudo element position

--- src/components/ParagraphBlock/index.jsx
+++ src/components/ParagraphBlock/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ParagraphBlock = (props) => {
     return (
-        <p className={`${props.bgColor === 'exist' ? 'bg-logoGray rounded-lg p-4' : props.bgColor} ${props.hidden === 'true' ? 'hidden' : props.hidden} text-lg tracking-wider leading-8`}>{props.paragraphContent}</p>
+        <p className={`${props.width} ${props.bgColor === 'exist' ? 'bg-logoGray rounded-lg p-4' : props.bgColor} ${props.hidden === 'true' ? 'hidden' : props.hidden} text-lg tracking-wider leading-8`}>{props.paragraphContent}</p>
     )
 }
 
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -13,17 +13,17 @@ const About = () => {
                
                 <HeadingTwo headingTwoContent="Time Is to Be Invested, Not Spent"></HeadingTwo>
 
-                <ParagraphBlock paragraphContent="DevNote is the only developer tool created to help you keep track of your projects, check what you have done and manage your time."></ParagraphBlock>
+                <ParagraphBlock width="lg:w-6/12" paragraphContent="DevNote is the only developer tool created to help you keep track of your projects, check what you have done and manage your time."></ParagraphBlock>
             </div>
 
             <div className="m-auto py-16 px-8 space-y-6 mb-10 tracking-wider leading-8 xl:px-20">
                 <HeadingTwo position="relative" headingTwoContent="The History Behind"></HeadingTwo>
 
-                <ParagraphBlock paragraphContent="In 2022 Eduardo Arakaki came up with the idea of creating a personal project management tool based on his need to keep track of his developer projects."></ParagraphBlock>
+                <ParagraphBlock width="lg:w-6/12" paragraphContent="In 2022 Eduardo Arakaki came up with the idea of creating a personal project management tool based on his need to keep track of his developer projects."></ParagraphBlock>
 
-                <ParagraphBlock paragraphContent="In the same year, he invested his time in creating mechanisms to bring the idea live. DevNote was born."></ParagraphBlock>
+                <ParagraphBlock width="lg:w-6/12" paragraphContent="In the same year, he invested his time in creating mechanisms to bring the idea live. DevNote was born."></ParagraphBlock>
 
-                <ParagraphBlock paragraphContent="Next year, in 2023, Eduardo invited Caroline Almeida Nikolic, a developer colleague, to participate in the project. They came up with the design and interactions and made Devnote available to the world."></ParagraphBlock>
+                <ParagraphBlock width="lg:w-6/12" paragraphContent="Next year, in 2023, Eduardo invited Caroline Almeida Nikolic, a developer colleague, to participate in the project. They came up with the design and interactions and made Devnote available to the world."></ParagraphBlock>
             </div>
         </main>
         )
