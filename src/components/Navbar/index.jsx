176a8a1490835b1668c18ed916ef8a7b2481b389
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-01-16 14:29:56 +0900

    feat: format navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -26,15 +26,16 @@ const Navbar = () => {
                     <img className='w-44' src={logo} alt="Devnote logo" />
 
                 </Link>
+                <div className='flex flex-row gap-5'>
                 {!user ?
-                    <div className="flex items-center md:order-2 text-base text-gray-700 font-bold hover:text-Orange">
+                    <div className="flex items-center md:order-2 text-base text-gray-700 font-bold hover:text-orange">
                         <Link to='/signin'>
                             Log In
                         </Link>
                     </div>
                     :
                     <div className="flex items-center md:order-2">
-                        <button type="button" className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom"
+                        <button type="button" className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-orange" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom"
                             onClick={() => {
                                 setAvatarMenu(!avatarMenu)
                                 setHamburgerMenu(false)
@@ -46,21 +47,21 @@ const Navbar = () => {
                                 <li>
                                     <Link to={
                                         !user ? '/signin' :
-                                            `/dashboard/user?:${user.uid}`}
+                                        `/dashboard/user?:${user.uid}`}
                                         className="block px-4 py-2 hover:bg-gray-100">Dashboard</Link>
                                 </li>
                                 <li>
                                     <Link to='/settings' className="block px-4 py-2 hover:bg-gray-100">Settings</Link>
                                 </li>
                                 <li>
-                                    <div className="block px-4 py-2 hover:bg-gray-100"
+                                    <div className="block px-4 py-2 hover:bg-gray-100 hover:cursor-pointer"
                                         onClick={() => handleSignOut()}>Sign out</div>
                                 </li>
                             </ul>
                         </div>
                     </div>
                 }
-                <button type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+                <button type="button" className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-orange"
                     onClick={() => {
                         setHamburgerMenu(!hamburgerMenu)
                         setAvatarMenu(false)
@@ -70,24 +71,25 @@ const Navbar = () => {
                 <div className={`${hamburgerMenu ? '' : 'hidden'} items-center justify-between w-full md:flex md:w-auto md:order-1`}>
                     <ul className="flex flex-col p-4 mt-4 font-bold text-base md:flex-row md:space-x-8 md:mt-0">
                         <li>
-                            <Link to='/' className="block py-2 pl-3 pr-4 hover:text-lightOrange text-gray-700 md:p-0" aria-current="page">Home</Link>
+                            <Link to='/' className="block py-2 pl-3 pr-4 hover:text-orange text-gray-700 md:p-0" aria-current="page">Home</Link>
                         </li>
                         <li>
                             <Link to={
                                 !user ? '/signin' :
-                                    `/dashboard/user?:${user.uid}`}
-                                className="block py-2 pl-3 pr-4 hover:text-lightOrange text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">Dashboard</Link>
+                                `/dashboard/user?:${user.uid}`}
+                                className="block py-2 pl-3 pr-4 hover:text-orange text-gray-700 md:p-0">Dashboard</Link>
                         </li>
                         <li>
-                            <Link to='/about' className="block py-2 pl-3 pr-4 hover:text-lightOrange text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">About</Link>
+                            <Link to='/about' className="block py-2 pl-3 pr-4 hover:text-orange text-gray-700 md:p-0">About</Link>
                         </li>
                         <li>
-                            <Link to='/contact' className="block py-2 pl-3 pr-4 hover:text-lightOrange text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">Contact</Link>
+                            <Link to='/contact' className="block py-2 pl-3 pr-4 hover:text-orange text-gray-700 md:p-0">Contact</Link>
                         </li>
                         <li>
-                            <Link to='/faq' className="block py-2 pl-3 pr-4 hover:text-lightOrange text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">FAQ</Link>
+                            <Link to='/faq' className="block py-2 pl-3 pr-4 hover:text-orange text-gray-700 md:p-0">FAQ</Link>
                         </li>
                     </ul>
+                                </div>
                 </div>
             </div>
         </nav>
