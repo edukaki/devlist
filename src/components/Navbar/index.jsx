6bdce02d2b364c141f2cd1b4fbdf0450fd7210db
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-01-16 13:59:34 +0900

    feat(navbar): change font size

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -20,14 +20,14 @@ const Navbar = () => {
 
     return (
 
-        <nav className="z-50 bg-white border-gray-200 px-2 w-full fixed sm:px-4 py-2.5 rounded font-courier">
+        <nav className="z-50 bg-white border-gray-200 px-2 w-full fixed sm:px-4 py-2.5 rounded">
             <div className="container flex flex-wrap items-center justify-between mx-auto">
                 <Link to='/' className="flex items-center">
                     <img className='w-44' src={logo} alt="Devnote logo" />
 
                 </Link>
                 {!user ?
-                    <div className="flex items-center md:order-2 text-lg text-gray-700 font-bold hover:text-Orange">
+                    <div className="flex items-center md:order-2 text-base text-gray-700 font-bold hover:text-Orange">
                         <Link to='/signin'>
                             Log In
                         </Link>
@@ -42,7 +42,7 @@ const Navbar = () => {
                             <img className="w-8 h-8 rounded-full" src={user.photoURL} alt="user" />
                         </button>
                         <div className={`${avatarMenu ? '' : 'hidden'} z-50 flex flex-col absolute justify-between w-full top-11 left-0 my-4 list-none bg-white divide-y divide-gray-100 rounded`} id="user-dropdown">
-                            <ul className="py-1 text-lg text-gray-700 font-bold">
+                            <ul className="py-1 text-base text-gray-700 font-bold">
                                 <li>
                                     <Link to={
                                         !user ? '/signin' :
@@ -68,7 +68,7 @@ const Navbar = () => {
                     <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                 </button>
                 <div className={`${hamburgerMenu ? '' : 'hidden'} items-center justify-between w-full md:flex md:w-auto md:order-1`}>
-                    <ul className="flex flex-col p-4 mt-4 font-bold text-lg md:flex-row md:space-x-8 md:mt-0">
+                    <ul className="flex flex-col p-4 mt-4 font-bold text-base md:flex-row md:space-x-8 md:mt-0">
                         <li>
                             <Link to='/' className="block py-2 pl-3 pr-4 hover:text-lightOrange text-gray-700 md:p-0" aria-current="page">Home</Link>
                         </li>
