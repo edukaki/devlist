41829339f66deff876c740319079b235594228d5
Carol A. Nikolic <carolnikolic@gmail.com>
2023-01-16 01:36:41 -0300

    feat(signin): add page elements

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 import GoogleButton from "react-google-button";
 import { useNavigate } from "react-router-dom";
 import { UserAuth } from "../../context/AuthContext";
+import InputGeneric from "../../components/InputGeneric";
+import ButtonGeneric from "../../components/ButtonGeneric";
 
 const SignIn = () => {
 
@@ -27,7 +29,15 @@ const SignIn = () => {
     return (
         <main>
             <div className="text-center text-3xl font-bold py-8">SignIn</div>
+            
+            <div>
+               <InputGeneric type="email" placeholder="Enter email"/>
+               <InputGeneric type="password" placeholder="Enter email"/>
+               <ButtonGeneric bgColor="bg-green" buttonName="Login"/>
+            </div>
+
             <div className="max-w-[240px] m-auto py-4">
+                <p>Or</p>
                 <GoogleButton onClick={handleGoogleSignIn} />
             </div>
         </main>
