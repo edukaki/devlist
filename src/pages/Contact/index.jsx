b5aa1335a05cb53f482df32b7f26d59a5001d1d6
Carol A. Nikolic <carolnikolic@gmail.com>
2023-01-17 14:46:28 -0300

    feat(contact): add position, spacing and alignment on containers

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ContactForm from '../../components/ContactForm'
+import HeadingOne from '../../components/HeadingOne'
+import HeadingTwo from '../../components/HeadingTwo'
+import ParagraphBlock from '../../components/ParagraphBlock'
+import ButtonGeneric from '../../components/ButtonGeneric'
 
 const Contact = () => {
     return (
         <main>
-            <ContactForm />
+
+            <div className="max-w-[550px] p-10 space-y-8">
+                <div className="pl-4 mb-8 bg-orange hover:bg-gradient-to-b from-orange to-green">
+                    <HeadingOne headingOneContent="Need anything? Get in touch. We can help!"/>
+                </div>
+
+                <div className="space-y-6 text-center md:text-left">
+                    <HeadingTwo headingTwoContent="Have any questions?"/>
+                    <ParagraphBlock paragraphContent="There is a chance somebody have already asked us."/>
+                    <ButtonGeneric buttonName="Go to FAQ"/>
+                </div>
+
+                <div>
+                    <HeadingTwo headingTwoContent="Or send us a message. We would love to hear from you."/>
+                    <ContactForm />
+                </div>
+            </div>
+
         </main>
     )
 }
