d879091b8a2ec3bc45947bf2a233561fc0db8183
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-01-17 13:36:07 +0900

    feat(dropdownQuestion): add function to hidden the paragraph

--- src/components/DropdownQuestion/index.jsx
+++ src/components/DropdownQuestion/index.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HeadingTwo from '../HeadingTwo';
 
 const DropdownQuestion = (props) => {
+    const[open,setOpen] = useState(false)
     return (
-            <button className="text-left">
+        <div>
+            <button className="text-left" onClick={() => setOpen(!open)}>
                 <HeadingTwo headingTwoContent={props.text}></HeadingTwo>
+                {open===false ?
                     <i className="fa-solid fa-caret-down"></i> :
                     <i className="fa-solid fa-caret-up"></i>
+                }
             </button>
+            <p className={`${open===false? 'hidden' : ''} ${props.bgColor === 'exist' ? 'bg-logoGray rounded-lg p-4' : props.bgColor} text-lg tracking-wider leading-8`}>{props.paragraphContent}</p>
+        </div>
     )
 }
 
