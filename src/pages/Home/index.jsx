e10b5b131f698471d28423182d8cbe8fa9b8b6ff
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-01-19 16:10:06 +0900

    feat(home): add background image

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import heroImgDesktop from '../../img/home/hero-img-desktop.png'
-import heroImgMobile from '../../img/home/hero-img-mobile.png'
 import ongoingIcon from '../../img/icons/ongoing-icon.png'
 import doneIcon from '../../img/icons/done-icon.png'
 import totalTimeIcon from '../../img/icons/clock-icon.png'
@@ -14,24 +12,18 @@ const Home = () => {
         <main>
 
             {/* Hero Information */}
-            <section className='flex flex-col justify-around text-darkGray md:relative'>
+            <section className="grid grid-flow-row h-full text-darkGray md:grid-cols-2 md:bg-cover md:h-[56vw] md:bg-heroImg-desktop">
 
-                {/* Hero Images (Art Direction) */}
-                <picture className="md:relative">
-                    <source media="(max-width: 767px)" srcSet={heroImgMobile}/>
-                    <source media="(min-width: 768px)" srcSet={heroImgDesktop}/>
-                    <img className='w-full' src={heroImgMobile} alt="Decorative hero"/>
-                </picture>
-
+                <div className='w-screen h-[100vw] bg-heroImg-mobile bg-cover md:hidden'/>
                 {/* Hero Text */}
-                <div className="flex flex-col tracking-wider m-auto font-roboto space-y-6 p-5 md:absolute md:top-10 md:left-10 lg:top-30 lg:left-20 xl:top-50 xl:left-30 2xl:top-50 2xl:left-40 2xl:pl-50 2xl:pt-25 min-[1800px]:top-52 min-[1800px]:left-52 min-[3000px]:top-60 min-[3000px]:left-72">
+                <div className="flex flex-col min-h-max tracking-wider m-auto space-y-6 p-5">
                     
-                    <div className="max-w-[400px] xl:max-w-[600px] min-[2500px]:max-w-[1400px]">
+                    <div className="xl:max-w-[600px] min-[2500px]:max-w-[1400px]">
                         <h1 className="font-courier font-bold text-4xl text-center pt-3 md:text-left lg:text-5xl xl:text-6xl min-[2500px]:text-8xl">DevNote</h1>
                         <p className="text-xl text-center m-auto pt-6 sm:w-2/3 md:text-left md:m-0 lg:text-3xl xl:text-4xl min-[2500px]:text-6xl">The ultimate <span className="font-courier uppercase font-bold text-orange">free</span> <span className="font-courier font-bold text-orange">developer</span> tool to keep track of your projects</p>
                     </div>
 
-                    <div className="max-w-[240px] space-y-6 mx-auto md:ml-0">
+                    <div className="max-w-[300px] space-y-6 mx-auto md:ml-0">
                         {/* Hero Email Input */}
                         <InputGeneric width="w-full" type="input" placeholder="email"></InputGeneric>
 
