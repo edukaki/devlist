3beb41cd26f59a294c6369a73865800bb8d55084
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-01-20 16:39:05 +0900

    feat(projectItem): add status indicator

--- src/components/ProjectItem/index.jsx
+++ src/components/ProjectItem/index.jsx
@@ -2,17 +2,26 @@ import React from 'react'
 
 const ProjectItem = (props) => {
     return (
-        <div className='grid grid-cols-4 text-center py-7 border border-b-orange'>
+        <div className='grid grid-cols-5 text-center py-7 border border-b-orange'>
+
             {props.check === true ?
                 <div className='flex flex-row gap-5 text-left'>
-                    <input type='checkbox' className='w-[2rem]'/>
+                    <input type='checkbox' className='w-[2rem]' />
                     <span>{props.project}</span>
                 </div> :
                 <span>{props.project}</span>
             }
-            <span>{props.client? props.client : 'Personal'}</span>
-            <span>{props.status}</span>
-            <span>{props.indicator}</span>
+            <span>{props.client ? props.client : 'Personal'}</span>
+            <span>{props.start}</span>
+            <span>{props.deadline}</span>
+
+            <span className='flex justify-center'>
+                {
+                    props.status === 'Active' ? <div className='w-6 h-6 bg-lime-500 rounded-full' /> :
+                        props.status === 'Paused' ? <div className='w-6 h-6 bg-red-500 rounded-full' /> :
+                            props.status === 'Closed' ? <div className='w-6 h-6 bg-gray-400 rounded-full' /> : props.indicator
+                }
+            </span>
         </div>
     )
 }
