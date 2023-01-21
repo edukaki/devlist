85e8bf412c4cd109cef53214e3452a985981df9f
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-01-21 14:41:55 +0900

    refactor: combine both types of heading into a single component

--- src/components/HeadingOne/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HeadingOne = (props) => {
-    return (
-        <h1 className="bg-gradient font-courier text-3xl font-bold text-darkGray pl-2 bg-lightGray">{props.headingOneContent}</h1>
-    )
-}
-
-export default HeadingOne
