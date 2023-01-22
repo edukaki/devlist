036ddc9adfc3fa292b538629bd9172804d194af7
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-01-22 19:29:09 +0900

    feat(projectCard): insert icon field

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -1,9 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
 const ProjectCard = (props) => {
     return (
         <div className={`grid grid-rows-4 rounded-lg p-6 max-w-full min-h-[200px] bg-gradient-to-br hover:scale-110 transition duration-500 ease-in-out hover:cursor-pointer ${props.fromColor} ${props.toColor} border-gray-400 shadow-lg`}>
-            <h2 className='row-span-2 text-2xl text-black font-bold tracking-wider'>{props.title}</h2>
+            <div className='flex flex-row gap-4'>
+                {!props.alt ? <FontAwesomeIcon icon={props.icon} size='2xl' /> : <img className='w-8 h-8' src={props.icon} alt={props.alt} />}
+                <h2 className='row-span-2 text-2xl text-black font-bold tracking-wider'>{props.title}</h2>
+            </div>
             <span className='row-span-1 text-3xl text-black font-bold tracking-wider'>{props.content}</span>
             <h4 className='row-span-1 text-l text-black tracking-wider'>{props.footer}</h4>
         </div>
