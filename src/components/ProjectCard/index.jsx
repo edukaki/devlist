b19ab356bc2066c63d134fc48a0483f7a2bacab4
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-01-23 16:44:07 +0900

    feat: format project card style

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const ProjectCard = (props) => {
     return (
-        <div className={`grid grid-rows-4 rounded-lg p-6 max-w-full min-h-[200px] bg-gradient-to-br hover:scale-110 transition duration-500 ease-in-out hover:cursor-pointer ${props.fromColor} ${props.toColor} border-gray-400 shadow-lg`}>
+        <div className={`grid grid-rows-3 items-center gap-2 rounded-lg p-6 max-w-full min-h-[200px] bg-gradient-to-br hover:scale-110 transition duration-500 ease-in-out hover:cursor-pointer ${props.fromColor} ${props.toColor} border-gray-400 shadow-lg`}>
             <div className='flex flex-row gap-4'>
                 {!props.alt ? <FontAwesomeIcon icon={props.icon} size='2xl' /> : <img className='w-8 h-8' src={props.icon} alt={props.alt} />}
                 <h2 className='row-span-2 text-2xl text-black font-bold tracking-wider'>{props.title}</h2>
