3921714e9e2b81247bc2507c5848615addb7dbb8
Carol A. Nikolic <carolnikolic@gmail.com>
2023-01-25 22:44:04 -0300

    feat(faq): add and position help image

--- src/pages/Faq/index.jsx
+++ src/pages/Faq/index.jsx
@@ -4,33 +4,39 @@ import Ruler from '../../components/Ruler'
 import faqImg from '../../img/faq/faq.png'
 import ContactForm from '../../components/ContactForm'
 import ParagraphBlock from '../../components/ParagraphBlock'
+import faqHelpImg from '../../img/faq/faq-help.png'
 
 const Faq = () => {
     return (
         <main>
+            <section className='flex flex-col md:flex-row'>
+                <div className="max-w-[550px] p-4 md:py-16 md:pl-20 pr-4 space-y-6 tracking-wider leading-8">
 
-            <div className="max-w-[550px] p-4 md:py-16 md:pl-20 pr-4 space-y-6 tracking-wider leading-8">
+                    <Heading line='left' type='headingOne' headingOneContent="Frequently Asked Questions" />
 
-                <Heading line='left' type='headingOne' headingOneContent="Frequently Asked Questions" />
+                    <DropdownQuestion text="How do I add my projects to DevNote?"
+                        bgColor="exist" paragraphContent="Add you project do DevNote by hitting the plus sign above your cards in your dashboard. Fill in the blank spaces and Devnote will sync automatically!" />
 
-                <DropdownQuestion text="How do I add my projects to DevNote?"
-                    bgColor="exist" paragraphContent="Add you project do DevNote by hitting the plus sign above your cards in your dashboard. Fill in the blank spaces and Devnote will sync automatically!" />
+                    <Ruler />
 
-                <Ruler />
+                    <DropdownQuestion text="How do DevNote keep track of the time spent on each project?"
+                        bgColor="exist" paragraphContent="In order to keep track of the time spent on each project, you simply need to change the status of your ongoing project from paused to in progress. DevNote will calcutae and display the invested time." />
 
-                <DropdownQuestion text="How do DevNote keep track of the time spent on each project?"
-                    bgColor="exist" paragraphContent="In order to keep track of the time spent on each project, you simply need to change the status of your ongoing project from paused to in progress. DevNote will calcutae and display the invested time." />
+                    <Ruler />
 
-                <Ruler />
+                    <DropdownQuestion text="Do I have to pay in order to use DevNote?"
+                        bgColor="exist" paragraphContent="No. DevNote is completely FREE!" />
+                    
+                    <Ruler />
 
-                <DropdownQuestion text="Do I have to pay in order to use DevNote?"
-                    bgColor="exist" paragraphContent="No. DevNote is completely FREE!" />
-                
-                <Ruler />
+                    <DropdownQuestion text="Why would my company use DevNote?"
+                        bgColor="exist" paragraphContent="This is an excelent question. If your company has a development team, then DevNote is for you. DevNote empowers the developers to keep track of the time spent on projects, so they can be more efficient and deliver on deadline." />
+                </div>
 
-                <DropdownQuestion text="Why would my company use DevNote?"
-                    bgColor="exist" paragraphContent="This is an excelent question. If your company has a development team, then DevNote is for you. DevNote empowers the developers to keep track of the time spent on projects, so they can be more efficient and deliver on deadline." />
-            </div>
+                <div className="max-w-[500px] p-2 mx-auto">
+                    <img src={faqHelpImg} alt="a person helping another"></img>
+                </div>
+            </section>
 
             <section className="flex flex-col justify-around md:flex-row max-w-[2000px] m-auto">
                 <h2 className="text-xl my-auto text-center tracking-wider md:text-start md:px-4 md:w-6/12 lg:text-2xl">Still wondering if <span className="font-courier text-orange">DevNote is the right</span> tool for you?</h2>
