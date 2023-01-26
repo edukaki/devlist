7232805b21002b623496fab8d793e1cc9a979e89
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-01-26 17:40:48 +0900

    fix((navbar): truncated menu layout

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -39,10 +39,12 @@ const Navbar = () => {
                             onClick={() => {
                                 setAvatarMenu(!avatarMenu)
                                 setHamburgerMenu(false)
-                            }}>
+                            }}
+                            onBlur={() => setAvatarMenu(false)}
+                            >
                             <img className="w-8 h-8 rounded-full" src={user.photoURL} alt="user" />
                         </button>
-                        <div className={`${avatarMenu ? '' : 'hidden'} z-50 flex flex-col absolute justify-between w-full top-11 left-0 my-4 list-none bg-white divide-y divide-gray-100 rounded`} id="user-dropdown">
+                        <div className={`${avatarMenu ? '' : 'hidden'} z-50 flex flex-col absolute justify-between w-full top-11 left-0 my-4 text-center list-none bg-white divide-y divide-gray-100 rounded`} id="user-dropdown">
                             <ul className="py-1 text-base text-gray-700 font-bold">
                                 <li>
                                     <Link to={
@@ -65,10 +67,11 @@ const Navbar = () => {
                     onClick={() => {
                         setHamburgerMenu(!hamburgerMenu)
                         setAvatarMenu(false)
-                    }}>
+                    }}
+                    onBlur={() => setHamburgerMenu(false)}>
                     <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                 </button>
-                <div className={`${hamburgerMenu ? '' : 'hidden'} items-center justify-between w-full md:flex md:w-auto md:order-1`}>
+                <div className={`${hamburgerMenu ? '' : 'hidden'} items-center justify-between w-full md:flex md:w-auto md:order-1 ${hamburgerMenu? 'absolute top-14 left-0 text-center  bg-white divide-y divide-gray-100 rounded' : ''}`}>
                     <ul className="flex flex-col p-4 mt-4 font-bold text-base md:flex-row md:space-x-8 md:mt-0">
                         <li>
                             <Link to='/' className="block py-2 pl-3 pr-4 hover:text-orange text-gray-700 md:p-0" aria-current="page">Home</Link>
