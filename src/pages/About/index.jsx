86811e7264ffb9536f4b579f55cdb84dcf3226dd
Carol A. Nikolic <carolnikolic@gmail.com>
2023-01-25 23:17:09 -0300

    feat(about): add new paragraphs

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -10,7 +10,9 @@ const About = () => {
 
                 <Heading type='headingTwo' headingTwoContent="Time Is to Be Invested, Not Spent"></Heading>
 
-                <ParagraphBlock width="lg:w-6/12" paragraphContent="DevNote is the only developer tool created to help you keep track of your projects, check what you have done and manage your time."></ParagraphBlock>
+                <ParagraphBlock width="lg:w-6/12" paragraphContent="DevNote is the only developer tool created to help you keep track of your projects, check what you have done and manage your time. An easy-to-use and intuitive platform where you add your projects and have an overview of your progress."></ParagraphBlock>
+                <ParagraphBlock width="lg:w-6/12" paragraphContent="We know that one of the biggest challenges companies face is delivering quality software within the deadline. If you are a company and want to assist and empower the developers to manage and optimize their time, DevNote is also for you."></ParagraphBlock>
+                <ParagraphBlock width="lg:w-6/12" paragraphContent="From conceptualization to the actual development, DevNote was created by developers and for developers. We know the importance of having our projects organized and to understand how much time we invested in each of them. By doing so we can improve and deliver efficiently."></ParagraphBlock>
             </div>
 
             <div className="m-auto p-4 md:py-16 md:pl-20 space-y-6 mb-10 tracking-wider leading-8 xl:px-20">
