da8479408e5d5dc3438c01a691064a6b934962f2
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-01-27 15:47:18 +0900

    feat(projectListItem): more descriptive icon

--- src/components/ProjectListItem/index.jsx
+++ src/components/ProjectListItem/index.jsx
@@ -10,15 +10,18 @@ const ProjectListItem = (props) => {
     const { btnAdd, setBtnAdd } = props.btnFunction
     return (
         <>
-            <div className='flex flex-row justify-end items-center gap-6 w-full px-3 mt-10 py-3 md:mt-0 md:px-6 bg-slate-400'>
-                <button className='relative' onClick={() => setBtnAdd(!btnAdd)}>
-                    <div className='w-5 h-1 rounded bg-slate-100' />
-                    <div className={`absolute top-0 w-5 h-1 rounded bg-slate-100 transition-all duration-500 ease-in-out ${!btnAdd ? 'rotate-90' : 'opacity-0'}`} />
+            <div className='flex flex-row justify-between items-center gap-6 w-full px-3 mt-10 md:mt-0 md:px-6 bg-slate-400'>
+                <button className='flex flex-row justify-end items-center gap-6 text-slate-100 font-bold' onClick={() => setBtnAdd(!btnAdd)}>
+                    <div className='relative'>
+                        <div className='w-5 h-1 rounded bg-slate-100' />
+                        <div className={`absolute top-0 w-5 h-1 rounded bg-slate-100 transition-all duration-500 ease-in-out ${!btnAdd ? 'rotate-90' : 'opacity-0'}`} />
+                    </div>
+                    <span>Add Project</span>
                 </button>
                 <button>
-                    <FontAwesomeIcon icon={faTrashCan} size='xl' color='#f1f5f9' />
-                </button>
-            </div>
+                        <FontAwesomeIcon icon={faTrashCan} size='xl' color='#f1f5f9' />
+                    </button>
+                </div>
             <div className='w-full px-3 mt-10 pb-20 md:mt-0 md:px-6 m-auto bg-slate-200'>
                 <div className='font-bold'><ProjectItem project='Project' client='Client' start='Start date' deadline='Deadline' indicator='Status' /></div>
                 {projectArr.map((project) => (
