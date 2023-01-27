9aaf03d1a70fd6cd5e7278201a3f885fd985ad5e
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-01-27 10:15:24 +0900

    feat(projectListItem): add tools bar

--- src/components/ProjectListItem/index.jsx
+++ src/components/ProjectListItem/index.jsx
@@ -3,25 +3,25 @@ import { UserProject } from '../../context/ProjectsContext'
 import ProjectItem from '../ProjectItem'
 
 
-const ProjectListItem = () => {
+const ProjectListItem = (props) => {
     const { projectArr } = UserProject()
     return (
-        <div>
-            {projectArr.map((project) => (
-                <ProjectItem
-                    key={project.key}
-                    check={true}
-                    project={project.project}
-                    client={project.client}
-                    status={project.status}
-                    start={project.start}
-                    deadline={project.deadline}
-                >
-                </ProjectItem>)
+            <div className='w-full px-3 mt-10 pb-20 md:mt-0 md:px-6 m-auto bg-slate-200'>
+                <div className='font-bold'><ProjectItem project='Project' client='Client' start='Start date' deadline='Deadline' indicator='Status' /></div>
+                {projectArr.map((project) => (
+                    <ProjectItem
+                        key={project.key}
+                        check={true}
+                        project={project.project}
+                        client={project.client}
+                        status={project.status}
+                        start={project.start}
+                        deadline={project.deadline}
+                    >
+                    </ProjectItem>)
                 )
-            }
-
-        </div>
+                }
+            </div>
     )
 }
 
