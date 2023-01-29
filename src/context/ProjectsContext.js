d496a302768070209cda736cb2828dbb78700577
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-01-29 13:43:25 +0900

    feat(projectsContext): include useDeleteData

--- src/context/ProjectsContext.js
+++ src/context/ProjectsContext.js
@@ -2,21 +2,29 @@ import { createContext, useState, useContext } from "react";
 import { UserAuth } from "./AuthContext";
 import useSetData from "../firebase/useSetData";
 import useGetData from "../firebase/useGetData";
+import useDeleteData from "../firebase/useDeleteData";
 
 const ProjectContext = createContext()
-
 
 export const ProjectContextProvider = ({ children }) => {
     const { user } = UserAuth()
     const [project, setProject] = useState(null)
-    const [projectArr, setProjectArr] = useState([])
+    const [projectArr, setProjectArr] = useState(null)
+    const [checkedItems, setCheckedItems] = useState([])
+    const [deleteItem, setDeleteItem] = useState(false)
 
     useSetData(project, user)
 
-    useGetData(user, setProjectArr, project)
+    useGetData(user, setProjectArr, project, projectArr)
+
+    useDeleteData(user,checkedItems,projectArr, deleteItem)
+
+    function handleDelete() {
+        setDeleteItem(true)
+    }
 
     return (
-        <ProjectContext.Provider value={{ setProject, projectArr }}>
+        <ProjectContext.Provider value={{ setProject, projectArr, checkedItems, setCheckedItems, handleDelete}}>
             {children}
         </ProjectContext.Provider>
     )
