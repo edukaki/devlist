2fe387537d3510f2c6dc47f38da51f5dff20ff2b
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-03 19:08:33 +0900

    fix(useDelete): after delete, the trigger remained enabled

--- src/firebase/useDeleteData.js
+++ src/firebase/useDeleteData.js
@@ -1,18 +1,20 @@
 import { deleteDoc, doc } from "firebase/firestore";
 import { db } from "./firebase";
 
-const useDeleteData = (user, checkedItems, projectArr, deleteItem) => {
+const useDeleteData = (user, checkedItems, arr, deleteItem, setDeleteItem) => {
     function handleDelete() {
         checkedItems.map((id) => {
             const deleteData = async () => {
                 const data = await doc(db, 'users', user.uid, 'projects', id);
                 deleteDoc(data)
             }
-            projectArr.map((item, index) => (
-                item.key === id && projectArr.splice(index, 1)
+
+            arr.map((item, index) => (
+                item.key === id && arr.splice(index, 1)
             ))
             return deleteData()
         })
+        setDeleteItem(false)
     }
     deleteItem && handleDelete()
 }
