5681ec97f7a83e92497653a2596abce8147af62d
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-03 18:56:02 +0900

    feat: create function to filter elements

---
+++ src/common/utils/Functions/filterArr.js
@@ -0,0 +1,7 @@
+const filterArr = (arr, filter) => {
+    let tempArr = []
+    arr.map((project) => filter.forEach(element => project.status === element && (tempArr = [...tempArr, project])))
+    return tempArr
+}
+
+export default filterArr
