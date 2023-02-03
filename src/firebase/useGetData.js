2007ce156aed787fe066d09003d41a6352e8bae6
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-03 19:14:59 +0900

    feat(useGetData): to fill sortedArr

--- src/firebase/useGetData.js
+++ src/firebase/useGetData.js
@@ -3,17 +3,21 @@ import { useEffect } from 'react'
 import sortArr from '../common/utils/Functions/sortArr';
 import { db } from './firebase'
 
-function useGetData(user, setProjectArr, project) {
+function useGetData(user, setProjectArr, project, setSortedArr) {
     useEffect(() => {
         const getData = async () => {
             const data = await getDocs(collection(db, 'users', user.uid, 'projects'));
-            setProjectArr(sortArr(data.docs.map((doc) => ({
+            
+            const arr = sortArr(data.docs.map((doc) => ({
                 ...doc.data(),
                 key: doc.data().key
-            })),"creation"));
+            })),"creation")
+
+            setProjectArr(arr);
+            setSortedArr(arr);
         };
         user.uid && getData();
-    }, [user,setProjectArr,project]);
+    }, [user,setProjectArr, setSortedArr, project]);
 }
 
 export default useGetData
