0fe0e9442df9d3bae08e1f55ca4de466d184efdf
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-03 14:40:55 +0900

    feat(useGetData): sort projectArr before render

--- src/firebase/useGetData.js
+++ src/firebase/useGetData.js
@@ -1,15 +1,16 @@
 import { collection, getDocs } from 'firebase/firestore'
 import { useEffect } from 'react'
+import sortArr from '../common/utils/Functions/sortArr';
 import { db } from './firebase'
 
 function useGetData(user, setProjectArr, project) {
     useEffect(() => {
         const getData = async () => {
             const data = await getDocs(collection(db, 'users', user.uid, 'projects'));
-            setProjectArr(data.docs.map((doc) => ({
+            setProjectArr(sortArr(data.docs.map((doc) => ({
                 ...doc.data(),
                 key: doc.data().key
-            })));
+            })),"creation"));
         };
         user.uid && getData();
     }, [user,setProjectArr,project]);
