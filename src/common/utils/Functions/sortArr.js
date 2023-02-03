5b856ad3b6d099c652c828706afcd12fcbadaf9f
Eduardo Arakaki <eduardo.arakaki@hotmail.com>
2023-02-03 18:57:10 +0900

    feat(sortArr): add order to sort items

--- src/common/utils/Functions/sortArr.js
+++ src/common/utils/Functions/sortArr.js
@@ -1,13 +1,13 @@
-const sortArr = (arr, filter) => {
+const sortArr = (arr, filter, order) => {
     
     return arr.sort((dataA,dataB) => {
         const projectA = filter === 'creation' ? dataA.creation : filter === 'project' ? dataA.project : filter === 'deadline' ? dataA.deadline : filter === 'start' ? dataA.start : ''
         const projectB = filter === 'creation' ? dataB.creation : filter === 'project' ? dataB.project : filter === 'deadline' ? dataB.deadline : filter === 'start' ? dataB.start : ''
         if (projectA > projectB) {
-            return 1
+            return order === 'ascending' ? 1 : -1
         }
         if (projectA < projectB) {
-            return -1
+            return order === 'ascending' ? -1 : 1
         }
         return 0
     })
